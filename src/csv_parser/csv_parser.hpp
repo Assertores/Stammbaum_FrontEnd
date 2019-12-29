9dc4f838d7ac421ad00373ab348b16e749ec5156
Green Sky <green@g-s.xyz>
2019-12-29 02:37:09 +0100

    impl most csv_parsing

--- src/csv_parser/csv_parser.hpp
+++ src/csv_parser/csv_parser.hpp
@@ -1,9 +1,73 @@
 #pragma once
 
+//#include <fstream>
 #include <string>
 #include <string_view>
+#include <tuple>
+#include <iomanip>
+#include <sstream>
+#include <cassert>
 
 namespace csv_parser {
 
+	template<typename T>
+	T parse_entry(std::string_view string);
+
+	template<typename T, typename... A>
+	std::tuple<T, A...> parse_line(std::string_view line_segment) {
+		auto comma_pos = line_segment.find_first_of(',');
+
+		if constexpr (sizeof...(A) > 0) {
+			assert(comma_pos != line_segment.npos);
+
+			return std::tuple_cat(
+					parse_line<T>(line_segment.substr(0, comma_pos)),
+					parse_line<A...>(line_segment.substr(comma_pos+1))
+				);
+		} else {
+			assert(comma_pos == line_segment.npos);
+
+			return std::tuple<T>(parse_entry<T>(line_segment));
+		}
+	}
+
+	template<>
+	std::string parse_entry(std::string_view string) {
+		return std::string{string};
+	}
+
+	template<>
+	bool parse_entry(std::string_view string) {
+		if (string == "true") {
+			return true;
+		} else if (string == "false") {
+			return false;
+		}
+
+		assert(false && "bool entry is neither");
+	}
+
+	template<>
+	uint64_t parse_entry(std::string_view string) {
+		size_t pos = 0;
+		return stoll(std::string{string}, &pos);
+	}
+
+	template<>
+	uint32_t parse_entry(std::string_view string) {
+		size_t pos = 0;
+		return stoll(std::string{string}, &pos);
+	}
+
+	template<>
+	std::tm parse_entry(std::string_view string) {
+		std::istringstream ss{std::string{string}};
+
+		std::tm tm;
+
+		ss >> std::get_time(&tm, "%d.%m.%Y");
+
+		return tm;
+	}
 }
 
--- src/csv_parser/test.cpp
+++ src/csv_parser/test.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <utility>
 
 #include "./csv_parser.hpp"
 
 int main(void) {
+
+	{
+		std::string line = "asdf,addd";
+
+		auto ret = csv_parser::parse_line<std::string, std::string>(line);
+
+		std::cout << "0: " << std::get<0>(ret) << "\n";
+		std::cout << "1: " << std::get<1>(ret) << "\n";
+	}
+
+	std::cout << std::string(40, '-') << "\n";
+
+	{
+		std::string line = "asdf,,addd";
+
+		auto ret = csv_parser::parse_line<std::string, std::string, std::string>(line);
+
+		std::cout << "0: " << std::get<0>(ret) << "\n";
+		std::cout << "1: " << std::get<1>(ret) << "\n";
+		std::cout << "2: " << std::get<2>(ret) << "\n";
+	}
+
+	std::cout << std::string(40, '-') << "\n";
+
+	{
+		std::string line = "asdf,false";
+
+		auto ret = csv_parser::parse_line<std::string, bool>(line);
+
+		std::cout << "0: " << std::get<0>(ret) << "\n";
+		std::cout << "1: " << (std::get<1>(ret) ? "true" : "false") << "\n";
+	}
+
+	std::cout << std::string(40, '-') << "\n";
+
+	{
+		std::string line = "asdf,false,05.06.1999";
+
+		auto ret = csv_parser::parse_line<std::string, bool, std::tm>(line);
+
+		std::cout << "0: " << std::get<0>(ret) << "\n";
+		std::cout << "1: " << (std::get<1>(ret) ? "true" : "false") << "\n";
+		std::cout << "2: " << std::put_time(&std::get<2>(ret), "%d-%m-%Y") << "\n";
+	}
 
 	return 0;
 }
