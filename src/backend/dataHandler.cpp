6e521ee3736bf6a44090849853fd965a032b525c
[JAM] Andreas <andreas.edmeier@web.de>
2020-01-22 15:00:35 +0100

    [PRO] datahandler namespace < static class

--- src/backend/dataHandler.cpp
+++ src/backend/dataHandler.cpp
@@ -7,53 +7,90 @@
 #include <csv_parser.hpp>
 #include "../utility/types_parser.hpp"
 
-void dataHandler::InitTables(const std::ifstream& peoples, const std::ifstream& titles, const std::ifstream& firstNames, const std::ifstream& lastNames, const std::ifstream& bloodLines, const std::ifstream& relationships) {
-	assert(!peoples.fail());
-	assert(!titles.fail());
-	assert(!firstNames.fail());
-	assert(!lastNames.fail());
-	assert(!bloodLines.fail());
-	assert(!relationships.fail());
+/// <summary>
+/// Table types
+/// int, bool, string, sexType, tm, string, tm, string, string
+/// </summary>
+void InitPeoplesTable(const std::ifstream& peoples);
 
-	InitPeoplesTable(peoples);
-	InitTitlesTable(titles);
-	InitFirstNamesTable(firstNames);
-	InitLastNamesTable(lastNames);
-	InitBloodLinesTable(bloodLines);
-	InitRelationshipsTable(relationships);
+/// <summary>
+/// Table types
+/// int, int, bool, string
+/// </summary>
+void InitTitlesTable(const std::ifstream& peoples);
+
+/// <summary>
+/// Table types
+/// int, int, bool, string
+/// </summary>
+void InitFirstNamesTable(const std::ifstream& peoples);
+
+/// <summary>
+/// Table types
+/// int, int, bool, string
+/// </summary>
+void InitLastNamesTable(const std::ifstream& peoples);
+
+/// <summary>
+/// Table types
+/// int, int, bool, relType
+/// </summary>
+void InitBloodLinesTable(const std::ifstream& peoples);
+
+/// <summary>
+/// Table types
+/// int, int, bool, relType, tm, tm
+/// </summary>
+void InitRelationshipsTable(const std::ifstream& peoples);
+
+namespace dataHandler {
+	void InitTables(const std::ifstream& peoples, const std::ifstream& titles, const std::ifstream& firstNames, const std::ifstream& lastNames, const std::ifstream& bloodLines, const std::ifstream& relationships) {
+		assert(!peoples.fail());
+		assert(!titles.fail());
+		assert(!firstNames.fail());
+		assert(!lastNames.fail());
+		assert(!bloodLines.fail());
+		assert(!relationships.fail());
+
+		InitPeoplesTable(peoples);
+		InitTitlesTable(titles);
+		InitFirstNamesTable(firstNames);
+		InitLastNamesTable(lastNames);
+		InitBloodLinesTable(bloodLines);
+		InitRelationshipsTable(relationships);
+	}
+
+	std::pair<std::set<blood>, std::set<relation>> GetRelations(const int rootID/* = 0*/, const int dinst/* = -1*/) {
+		assert(false && "not implimented");
+		return std::pair<std::set<blood>, std::set<relation>>();
+	}
+
+	personInfos GetPerson(const int personID) {
+		assert(false && "not implimented");
+		return personInfos();
+	}
 }
 
-std::pair<std::set<blood>, std::set<relation>> dataHandler::GetRelations(const int rootID/* = 0*/, const int dinst/* = -1*/) {
+void InitPeoplesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
-	return std::pair<std::set<blood>, std::set<relation>>();
 }
 
-personInfos dataHandler::GetPerson(const int personID){
-	assert(false && "not implimented");
-	return personInfos();
-}
-
-void dataHandler::InitPeoplesTable(const std::ifstream& peoples){
+void InitTitlesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void dataHandler::InitTitlesTable(const std::ifstream& peoples){
+void InitFirstNamesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void dataHandler::InitFirstNamesTable(const std::ifstream& peoples){
+void InitLastNamesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void dataHandler::InitLastNamesTable(const std::ifstream& peoples){
+void InitBloodLinesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void dataHandler::InitBloodLinesTable(const std::ifstream& peoples){
+void InitRelationshipsTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
-
-void dataHandler::InitRelationshipsTable(const std::ifstream& peoples){
-	assert(false && "not implimented");
-}
-
--- src/backend/dataHandler.h
+++ src/backend/dataHandler.h
@@ -8,12 +8,11 @@
 #include "../utility/types.h"
 
 
-class dataHandler{
-public:
+namespace dataHandler {
 	/// <summary>
 	/// initialices all tables at once. asserts all table files are opend
 	/// </summary>
-	static void InitTables(const std::ifstream& peoples, const std::ifstream& titles, const std::ifstream& firstNames, const std::ifstream& lastNames, const std::ifstream&bloodLines, const std::ifstream& relationships);
+	void InitTables(const std::ifstream& peoples, const std::ifstream& titles, const std::ifstream& firstNames, const std::ifstream& lastNames, const std::ifstream& bloodLines, const std::ifstream& relationships);
 
 	/// <summary>
 	/// retreave a subtree of the data. retreaves the whole tree or even multiple trees.
@@ -21,50 +20,13 @@ public:
 	/// <param rootID = 0>the id of the person in question</param>
 	/// <param dinst = -1>the max distance of relationships removed from the person</param>
 	/// <return>a list of all relevant relations</return>
-	static std::pair<std::set<blood>, std::set<relation>> GetRelations(const int rootID = 0, const int dinst = -1);
+	std::pair<std::set<blood>, std::set<relation>> GetRelations(const int rootID = 0, const int dinst = -1);
 
 	/// <summary>
 	/// you know. SQL and so ...
 	/// </summary>
 	/// <return>all relevant informations of an specific person</return>
-	static personInfos GetPerson(const int personID);
+	personInfos GetPerson(const int personID);
 
-private:
-
-	/// <summary>
-	/// Table types
-	/// int, bool, string, sexType, tm, string, tm, string, string
-	/// </summary>
-	static void InitPeoplesTable(const std::ifstream& peoples);
-
-	/// <summary>
-	/// Table types
-	/// int, int, bool, string
-	/// </summary>
-	static void InitTitlesTable(const std::ifstream& peoples);
-
-	/// <summary>
-	/// Table types
-	/// int, int, bool, string
-	/// </summary>
-	static void InitFirstNamesTable(const std::ifstream& peoples);
-
-	/// <summary>
-	/// Table types
-	/// int, int, bool, string
-	/// </summary>
-	static void InitLastNamesTable(const std::ifstream& peoples);
-
-	/// <summary>
-	/// Table types
-	/// int, int, bool, relType
-	/// </summary>
-	static void InitBloodLinesTable(const std::ifstream& peoples);
-
-	/// <summary>
-	/// Table types
-	/// int, int, bool, relType, tm, tm
-	/// </summary>
-	static void InitRelationshipsTable(const std::ifstream& peoples);
-};
-
+	
+} //dataHandler
