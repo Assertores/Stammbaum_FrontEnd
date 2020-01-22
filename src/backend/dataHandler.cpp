c359d47b213c7b3f1b1ca875c040ce7cab68d362
Green Sky <green@g-s.xyz>
2020-01-22 15:37:24 +0100

    prevent linker leakage

--- src/backend/dataHandler.cpp
+++ src/backend/dataHandler.cpp
@@ -11,37 +11,37 @@
 /// Table types
 /// int, bool, string, sexType, tm, string, tm, string, string
 /// </summary>
-void InitPeoplesTable(const std::ifstream& peoples);
+static void InitPeoplesTable(const std::ifstream& peoples);
 
 /// <summary>
 /// Table types
 /// int, int, bool, string
 /// </summary>
-void InitTitlesTable(const std::ifstream& peoples);
+static void InitTitlesTable(const std::ifstream& peoples);
 
 /// <summary>
 /// Table types
 /// int, int, bool, string
 /// </summary>
-void InitFirstNamesTable(const std::ifstream& peoples);
+static void InitFirstNamesTable(const std::ifstream& peoples);
 
 /// <summary>
 /// Table types
 /// int, int, bool, string
 /// </summary>
-void InitLastNamesTable(const std::ifstream& peoples);
+static void InitLastNamesTable(const std::ifstream& peoples);
 
 /// <summary>
 /// Table types
 /// int, int, bool, relType
 /// </summary>
-void InitBloodLinesTable(const std::ifstream& peoples);
+static void InitBloodLinesTable(const std::ifstream& peoples);
 
 /// <summary>
 /// Table types
 /// int, int, bool, relType, tm, tm
 /// </summary>
-void InitRelationshipsTable(const std::ifstream& peoples);
+static void InitRelationshipsTable(const std::ifstream& peoples);
 
 namespace dataHandler {
 	void InitTables(const std::ifstream& peoples, const std::ifstream& titles, const std::ifstream& firstNames, const std::ifstream& lastNames, const std::ifstream& bloodLines, const std::ifstream& relationships) {
@@ -71,26 +71,27 @@ namespace dataHandler {
 	}
 }
 
-void InitPeoplesTable(const std::ifstream& peoples) {
+static void InitPeoplesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void InitTitlesTable(const std::ifstream& peoples) {
+static void InitTitlesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void InitFirstNamesTable(const std::ifstream& peoples) {
+static void InitFirstNamesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void InitLastNamesTable(const std::ifstream& peoples) {
+static void InitLastNamesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void InitBloodLinesTable(const std::ifstream& peoples) {
+static void InitBloodLinesTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
 
-void InitRelationshipsTable(const std::ifstream& peoples) {
+static void InitRelationshipsTable(const std::ifstream& peoples) {
 	assert(false && "not implimented");
 }
+
