c033594fc7194bb2093f386e303869ea51126f31
[JAM] Andreas <andreas.edmeier@web.de>
2020-02-08 18:54:54 +0100

    [DOC] types.h in doxygen style

--- src/utility/types.h
+++ src/utility/types.h
@@ -10,12 +10,13 @@
 #include "../frontend/treeBuilderElement.h"
 #include "../frontend/MLMSElement.h"
 
-typedef const char* cString;
-typedef std::map<int, treeBuilderElement> tree;
-typedef std::vector<std::vector<int>> generations;
-typedef std::pair<std::set<int>, std::set<int>> family;
-typedef std::vector<std::pair<int, MLMSElement>> visGen;//visual Generation
+typedef const char* cString;								//!< @typedef c-style strings
+typedef std::map<int, treeBuilderElement> tree;				//!< @typedef relational tree
+typedef std::vector<std::vector<int>> generations;			//!< @typedef 2DArray of personIDs
+typedef std::pair<std::set<int>, std::set<int>> family;		//!< @typedef set of parentIDs and set of ChildrenIDs
+typedef std::vector<std::pair<int, MLMSElement>> visGen;	//!< @typedef visual Generation containing all informations to draw a generation
 
+/*! relation Type */
 enum relType {
 	Father,
 	Mother,
@@ -23,39 +24,47 @@ enum relType {
 	Married,
 };
 
+/*! sex Type */
 enum sexType {
 	Male,
 	Female,
 	Deverce,
 };
 
+/*! relations by bloud that will be translated in parent-children relations */
 struct blood {
-	int objectID;
-	int subjectID;
-	relType type;
+	int objectID;	//!< parent
+	int subjectID;	//!< child
+	relType type;	//!< \p objectID is \p type of \p subjectID
 
+	/*! needed for map */
 	bool operator < (const blood& rhs) const {
 		return (objectID != rhs.objectID) ? objectID < rhs.objectID : subjectID < rhs.subjectID;
 	}
 };
 
+/*! artefitial relations between to people e.g. married, friend */
 struct relation {
 	int objectID;
 	int subjectID;
 	relType type;
-	std::tm start;
-	std::tm end;
+	std::tm start;	//!< start time of the relation
+	std::tm end;	//!< end time of the relation (divorce, etc.)
 
+	/*! needed for map */
 	bool operator < (const blood& rhs) const {
 		return (objectID != rhs.objectID) ? objectID < rhs.objectID : subjectID < rhs.subjectID;
 	}
 };
 
+/*!
+ * all informations regarding to a person
+ */
 struct personInfos {
-	std::vector<std::string> titles;
-	std::vector<std::string> firstNames;
-	std::string suffix;
-	std::vector<std::string> lastNames;
+	std::vector<std::string> titles;		//!< all titles (Dr., Prof., etc.)
+	std::vector<std::string> firstNames;	//!< all first names
+	std::string suffix;						//!< e.g. von und zu
+	std::vector<std::string> lastNames;		//!< all last names e.g. from previos mareys
 
 	sexType sex;
 
